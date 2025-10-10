20c361b91eb6352aa76b9dc9f99a0060e3851f80
Sujith1-cntr <sujithpz339@gmail.com>
2025-10-10 18:14:04 +0530

    Update variables.tf
    defined variable according to docker

--- variables.tf
+++ variables.tf
@@ -1,9 +1,23 @@
-variable "subscription_id" { type = string }
-variable "tenant_id"       { type = string }
-variable "client_id"       { type = string }
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+  default     = "aca-rg"
+}
 
-variable "rg_name"   { type = string }
-variable "location"  { type = string  default = "eastus" }
-variable "plan_name" { type = string  default = "asp-tomcat-demo" }
-variable "plan_sku"  { type = string  default = "B1" }
-variable "app_name"  { type = string }
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "env_name" {
+  description = "Container App Environment name"
+  type        = string
+  default     = "aca-env"
+}
+
+variable "app_name" {
+  description = "Container App name"
+  type        = string
+  default     = "tomcat-aca"
+}
